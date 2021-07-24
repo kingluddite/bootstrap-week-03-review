3f879a03baf59dca28dda804634b718d53720b09
philip howley <howley.phil@gmail.com>
2021-07-24 11:42:06 -0400

    Add javascript review

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,56 @@
-console.log('js is working');
+const age = 0;
 
-const myBtnEl = document.querySelector('.my-button');
-const myTxtEl = document.querySelector('.my-text');
-
-function swapSomeText() {
-   // replace text
-   myTxtEl.textContent = myBtnEl.getAttribute('data-whatever');
-   
+if (age) {
+ console.log('you can enter the bar');
+} else {
+ console.log('sorry you can not enter the bar');
 }
 
-// TODO add event to capture click
-myBtnEl.addEventListener('click', swapSomeText);
+// const daysOfWeek = [
+//   'Monday',
+//   'Tuesday',
+//   'Wednesday',
+//   'Thursday',
+//   'Friday',
+//   'Saturday',
+//   'PhilDay',
+//   'PhilDayAgain'
+// ];
+// // daysOfWeek.push('Sunday');
+// // console.log(daysOfWeek[3]);
+
+// for (let i = 0; i < daysOfWeek.length; i++) {
+//    console.log(daysOfWeek[i]);
+// }
+
+// // const age = 7;
+// // const likesSoccer = true;
+// // const firstName = 'Phil';
+// // const lastName = 'Howley';
+// // let score = null;
+// // console.log(score);
+
+// const person = {
+//    firstName: 'phil',
+//    lastName: 'howley',
+//    age: 21
+// }
+
+// console.log(person);
+
+// const fullName = `${firstName} ${lastName}`;
+// console.log(fullName);
+
+// console.log('js is working');
+
+// const myBtnEl = document.querySelector('.my-button');
+// const myTxtEl = document.querySelector('.my-text');
+
+// function swapSomeText() {
+//    // replace text
+//    myTxtEl.textContent = myBtnEl.getAttribute('data-whatever');
+
+// }
+
+// // TODO add event to capture click
+// myBtnEl.addEventListener('click', swapSomeText);
